56c7e98eeb55dba367b9a058f0b5fa8c3191b81b
DnYontz <DYontz8981@my.sctd.edu>
2017-01-03 20:53:05 -0500

    Added Data Types

--- CrashCourse/Program.cs
+++ CrashCourse/Program.cs
@@ -11,7 +11,22 @@ namespace CrashCourse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string name = "Danielle";
+            int number = 25;
+            char letter = 'a';
+            double doubleNumber = 5.5;
+            bool myBoolean = true;
+
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("Letter: " + letter);
+            Console.WriteLine("Decimal Number: " + doubleNumber);
+            Console.WriteLine("Boolean: " + myBoolean);
             Console.Read();
+
+
         }
     }
 }
