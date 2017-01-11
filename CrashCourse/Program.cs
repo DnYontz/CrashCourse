258e30ad78fde7f16d7b26a548b2cf738d44a4ae
DnYontz <DYontz8981@my.sctd.edu>
2017-01-10 20:00:08 -0500

    Fizz Buzz Decision Strucutre Added

--- CrashCourse/Program.cs
+++ CrashCourse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,36 +12,100 @@ namespace CrashCourse
         static void Main(string[] args)
         {
 
-            // Give the user directions (1-5)
-            Console.WriteLine("Pick a number (1-5):");
-
-            // Set winning numbers
-            const int winningNumber1 = 3;
-            const int winningNumber2 = 5;
-
-            // Get data from the user
-            String userData = Console.ReadLine();
-
-            // Convert it to int
-            int userValue;
-            int.TryParse(userData, out userValue);
-            
-            // Do they match?
-            switch(userValue)
+            for (int i = 1; i < 101; i++)
             {
-                case winningNumber1:
-                    Console.WriteLine("You Win!");
-                    break;
-                case winningNumber2:
-                    Console.WriteLine("You kinda win!");
-                    break;
-                default:
-                    Console.WriteLine("You definitely lose.");
-                    break;
+                int divisibleThree = i % 3;
+                int divisibleFive = i % 5;
+                int divisibleBoth = i % 15;
+
+                if (divisibleThree == 0)
+                {
+
+                    if (divisibleBoth == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else
+                {
+                    if (divisibleFive == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                    }
+
             }
 
 
 
+
+
+
+
+            //List<string> listOfStuff = new List<string>();
+            //List<int> listOfNumber = new List<int>();
+
+            //ArrayList listOfStuff = new ArrayList();
+            //listOfStuff.Add("Thing-0");
+            //listOfStuff.Add("Thing-1");
+            //listOfStuff.Add("Thing-2");
+            //listOfStuff.Add("Thing-3");
+            //listOfStuff.Add("Thing-4");
+
+            //foreach (string thing in listOfStuff)
+            //{
+            //    Console.WriteLine(thing);
+            //}
+            //Console.WriteLine(listOfStuff.Count);
+
+            //int destroyIndex = listOfStuff.IndexOf("Thing-1");
+            //listOfStuff.RemoveAt(destroyIndex);
+
+            //Console.WriteLine(listOfStuff.Count);
+
+            //string[] arrayOfStuff = new string[5];
+            //arrayOfStuff[0] = "Thing-0";
+            //arrayOfStuff[1] = "Thing-1";
+            //arrayOfStuff[2] = "Thing-2";
+            //arrayOfStuff[3] = "Thing-3";
+            //arrayOfStuff[4] = "Thing-4";
+
+            //for (int i = 0; i < arrayOfStuff.Length; i++)
+            //{
+            //    Console.WriteLine(arrayOfStuff[i]);
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Hello World");
+            //}
+
+
+            //int counter = 5;
+
+            //do
+            //{
+            //    Console.WriteLine("Hello World");
+            //    counter--;
+            //}
+            //while (counter > 5);
+
+
+
+
+            //int counter = 5;
+            //while (counter > 0)
+            //{
+            //    Console.WriteLine("Hello World");
+            //    counter--;
+            //}
 
 
 
