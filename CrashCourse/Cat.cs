330d292f1ffb986024e1ce6684fafbc69c8a8750
DnYontz <DYontz8981@my.sctd.edu>
2017-01-17 17:57:44 -0500

    Cat and Dog Class Created

---
+++ CrashCourse/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourse
+{
+
+    class Cat
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public bool isFixed { get; set; }
+        public double Weight { get; set; }
+        public string Color { get; set; }
+
+        public Cat(int Age, string Name, char Genger, bool isFixed)
+        {
+            this.Name = " ";
+            this.Gender = '?';
+            this.isFixed = false;
+            this.Weight = Weight;
+            this.Color = "Orange";
+        }
+        public string MakeNoise()
+        {
+            return "Meow";
+        }
+
+        public string Move()
+        {
+            return "Walking....";
+        }
+        public string isHappy()
+        {
+            return "Purr";
+        }
+
+    }
+}
---
+++ CrashCourse/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourse
+{
+    class Dog
+    {
+        public char Gender { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool isFixed { get; set; }
+        public double Weight { get; set; }
+        public string Color { get; set; }
+
+        public Dog(int Age, char Gender, string Name, bool isFixed)
+        {
+            this.Name = " ";
+            this.Gender = '?';
+            this.isFixed = false;
+            this.Weight = Weight;
+            this.Color = "Brown";
+        }
+
+        public string MakeNoise()
+        {
+            return "Woof";
+        }
+        public string Move()
+        {
+            return "Walking...";
+        }
+        public string isHappy()
+        {
+            return "Wagging Tail...";
+
+        }
+
+    }
+}
